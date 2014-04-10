cdf4901ea8d8efc581b6db997bdd8564cd48f361
Arjen Hiemstra <arjenh@me.com>
2014-04-10 23:55:15 +0200

    Update w5200.c

--- hardware/ariadne/bootloaders/ariadne/w5200.c
+++ hardware/ariadne/bootloaders/ariadne/w5200.c
@@ -72,8 +72,29 @@ void netInit(void)
 	)
 
 	/** Configure Wiznet chip. Network settings */
-	for(i = 0; i < REGISTER_BLOCK_SIZE; i++)
-		spiWriteReg(i, registerBuffer[i]);
+//	for(i = 0; i < REGISTER_BLOCK_SIZE; i++)
+//		spiWriteReg(i, registerBuffer[i]);
+
+		spiWriteReg(0, registerBuffer[0]);
+
+                for (int i=0; i<8; i++) {
+                  spiWriteReg((0x4000 + i * 0x100 + 0x001F), 2);
+                  spiWriteReg((0x4000 + i * 0x100 + 0x001E), 2);
+                }
+                
+		for(i = 9; i < 15; i++) {
+			spiWriteReg(i, registerBuffer[i]);
+		}
+		for(i = 15; i < 19; i++) {
+			spiWriteReg(i, registerBuffer[i]);
+		}
+		for(i = 1; i < 5; i++) {
+			spiWriteReg(i, registerBuffer[i]);
+		}
+		for(i = 5; i < 9; i++) {
+			spiWriteReg(i, registerBuffer[i]);
+		}
+		
 
 	DBG_NET(tracePGMlnNet(mDebugNet_DONE);)
 }
