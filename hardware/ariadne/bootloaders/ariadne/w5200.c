05a3030e6da03e3dc8dd81943572e66c9018474e
Arjen Hiemstra <arjenh@me.com>
2014-04-11 16:32:26 +0200

    Update w5200.c

--- hardware/ariadne/bootloaders/ariadne/w5200.c
+++ hardware/ariadne/bootloaders/ariadne/w5200.c
@@ -75,24 +75,24 @@ void netInit(void)
 //	for(i = 0; i < REGISTER_BLOCK_SIZE-2; i++)
 //		spiWriteReg(i, registerBuffer[i]);
 
-		spiWriteReg(0, registerBuffer[0]);
+		spiWriteReg(0, 0, registerBuffer[0]);
 
                 for (i=0; i<8; i++) {
-                  spiWriteReg((0x4000 + i * 0x100 + 0x001F), 0x02);
-                  spiWriteReg((0x4000 + i * 0x100 + 0x001E), 0x02);
+                  spiWriteReg((0x4000 + i * 0x100 + 0x001F), 0, 0x02);
+                  spiWriteReg((0x4000 + i * 0x100 + 0x001E), 0, 0x02);
                 }
                 
 		for(i = 9; i < 15; i++) {
-			spiWriteReg(i, registerBuffer[i]);
+			spiWriteReg(i, 0, registerBuffer[i]);
 		}
 		for(i = 15; i < 19; i++) {
-			spiWriteReg(i, registerBuffer[i]);
+			spiWriteReg(i, 0, registerBuffer[i]);
 		}
 		for(i = 1; i < 5; i++) {
-			spiWriteReg(i, registerBuffer[i]);
+			spiWriteReg(i, 0, registerBuffer[i]);
 		}
 		for(i = 5; i < 9; i++) {
-			spiWriteReg(i, registerBuffer[i]);
+			spiWriteReg(i, 0, registerBuffer[i]);
 		}
 		
 
