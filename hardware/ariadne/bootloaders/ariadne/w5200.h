d2cfb304d68dca3b8513983e0c3242dcca42e175
Arjen Hiemstra <arjenh@me.com>
2014-04-11 15:43:50 +0200

    Update w5200.h

--- hardware/ariadne/bootloaders/ariadne/w5200.h
+++ hardware/ariadne/bootloaders/ariadne/w5200.h
@@ -285,14 +285,6 @@
 //#define IR_CLR       0x00
 
 
-/* Network settings */
-#define IP_ADDR     192,168,84,128
-#define SUB_MASK    255,255,255,0
-#define GW_ADDR     192,168,84,1
-#define MAC_ADDR    0xDE,0xAD,0xBE,0xEF,0xFE,0xED
-
-
-
 void netInit(void);
 
 
