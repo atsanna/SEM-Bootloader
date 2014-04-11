0c30de7750927783793e1d445f6abcd74ba7901a
Arjen Hiemstra <arjenh@me.com>
2014-04-11 12:21:40 +0200

    Update debug_main.h

--- hardware/ariadne/bootloaders/ariadne/debug_main.h
+++ hardware/ariadne/bootloaders/ariadne/debug_main.h
@@ -20,16 +20,16 @@ const unsigned char mDebugMain_PREFIX[]	PROGMEM = 	"Main: ";
 
 	#if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
 		#if defined(ARDUINO_ETHERNET)
-const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"Ariadne for Arduino Ethernet, Version 0.5";
+const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"SEMBoot for Arduino Ethernet, Version 0.6";
 		#else
-const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"Ariadne for Arduino Uno, Version 0.5";
+const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"SEMBoot for Arduino Uno, Version 0.6";
 		#endif
 	#elif defined(__AVR_ATmega2560__)
-const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"Ariadne for Arduino Mega2560, Version 0.5";
+const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"SEMBoot for Arduino Mega2560, Version 0.6";
 	#elif defined(__AVR_ATmega1284P__)
-const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"Ariadne for Arduino Mega1284P, Version 0.5";
+const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"SEMBoot for Arduino Mega1284P, Version 0.6";
 	#else
-const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"Unknown MCU with ariadne, Version 0.5";
+const unsigned char mDebugMain_TITLE[]	PROGMEM = 	"Unknown MCU with ariadne, Version 0.6";
 	#endif
 const unsigned char mDebugMain_EXIT[]	PROGMEM = 	"Start user app";
 	#if (DEBUG_MAIN > 1)
